b6beb9c44039b6e5d8b5be08882494bf5e3e44dc
zzflybird <1069732181@qq.com>
2022-05-27 10:52:02 +0800

    配置类，配置了Bean的扫描路径

--- src/main/java/com/zzflybird/AppConfig.java
+++ src/main/java/com/zzflybird/AppConfig.java
@@ -7,9 +7,12 @@ package com.zzflybird;
  * @version: 1.0
  */
 
+import com.spring.ComponentScan;
+
 /**
  * Config配置类
  */
-
+@ComponentScan(value = "com.zzflybird.service") // 配置Bean扫描
 public class AppConfig {
+
 }
