7387ecbfb661e13e8b21764fe1583f63760ddd19
zzflybird <1069732181@qq.com>
2022-05-27 10:55:08 +0800

    Bean

---
+++ src/main/java/com/zzflybird/service/OrderService.java
@@ -0,0 +1,13 @@
+package com.zzflybird.service;
+
+import com.spring.Component;
+
+/**
+ * @Auther: zzflybird
+ * @Date: 2022/5/26 - 05 - 26 - 20:42
+ * @Description: com.zzflybird.service
+ * @version: 1.0
+ */
+
+public class OrderService {
+}
