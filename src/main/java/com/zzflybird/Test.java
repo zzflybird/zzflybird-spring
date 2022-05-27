7074ebe5024f61ca336475163ef66e3de4a7d455
zzflybird <1069732181@qq.com>
2022-05-27 11:01:11 +0800

    测试类，测试Spring的功能：扫描，getBean()，单例，原型等.

--- src/main/java/com/zzflybird/Test.java
+++ src/main/java/com/zzflybird/Test.java
@@ -1,5 +1,8 @@
 package com.zzflybird;
 
+import com.spring.ZzflybirdApplicationContext;
+import com.zzflybird.service.UserService;
+
 /**
  * @Auther: zzflybird
  * @Date: 2022/5/26 - 05 - 26 - 20:14
@@ -7,7 +10,15 @@ package com.zzflybird;
  * @version: 1.0
  */
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
+        // 1 扫描---》2 创建单例Bean
+        ZzflybirdApplicationContext context = new ZzflybirdApplicationContext(AppConfig.class);
+
+        UserService userService = (UserService) context.getBean("userService");
+        System.out.println(context.getBean("userService"));
+        System.out.println(context.getBean("userService"));
+        System.out.println(context.getBean("userService"));
+        userService.test();
     }
 }
