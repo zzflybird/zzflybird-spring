f70619f81da197933e1129fe392cef9280ce11d7
zzflybird <1069732181@qq.com>
2022-05-27 10:54:08 +0800

    Component注解，用于声明Bean，表明这是一个Bean对象，这样才能被Spring扫描到

--- src/main/java/com/spring/Component.java
+++ src/main/java/com/spring/Component.java
@@ -7,9 +7,18 @@ package com.spring;
  * @version: 1.0
  */
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Component注解，用于声明bean
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface Component {
+
+    String value() default "";
 
 }
